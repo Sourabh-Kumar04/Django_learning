68f122725ecd8fe81d69d6c48e103f19579fbbfd
Sourabh-Kumar04 <sourabhkumar01010101@gmail.com>
2024-05-23 23:19:45 +0530

    Tailwind L4 complete

--- RasoDjango/settings.py
+++ RasoDjango/settings.py
@@ -40,7 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'raso',
     'tailwind',
+    'theme',
+    'django_browser_reload',
 ]
+
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS = ['127.0.0.1']
+
+NPM_BIM_PATH = '/mnt/c\Program Files\nodejs\npm'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -50,6 +57,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'RasoDjango.urls'
--- RasoDjango/urls.py
+++ RasoDjango/urls.py
@@ -23,5 +23,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
-    path('raso/', include('raso.urls'))
+    path('raso/', include('raso.urls')),
+
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
