3e29b028ee7d6feeb1ecc63a22dc19de741e6239
Sourabh-Kumar04 <sourabhkumar01010101@gmail.com>
2024-05-31 03:15:52 +0530

    relationship model continue

--- raso/admin.py
+++ raso/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
-from .models import RasoVarity
+from .models import RasoVarity, RasoReview, Store, RasoCertificate
 
 # Register your models here.
-admin.site.register(RasoVarity)
+class RasoReviewInline(admin.TabularInline):
+    model = RasoReview
+    extra = 2
+
+class RasoVarietyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'date_added')
+    inlines = [RasoReviewInline]
+
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    # filter_horizontal = ('raso_varities',)
+
+class RasoCertificateAdmin(admin.ModelAdmin):
+    list_display = ('raso', 'certificate_number', 'issue_date', 'valid_untill')    
+
+
+admin.site.register(RasoVarity, RasoVarietyAdmin)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(RasoCertificate, RasoCertificateAdmin)
--- raso/models.py
+++ raso/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class RasoVarity(models.Model):
@@ -12,11 +13,49 @@ class RasoVarity(models.Model):
     ]
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='rasos/')
-    date_addes = models.DateTimeField(default=timezone.now)
+    date_added = models.DateTimeField(default=timezone.now)
     type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICE)
     description = models.TextField(default='')
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.name
-    
+
+# One to Many
+class RasoReview(models.Model) :
+    raso = models.ForeignKey(RasoVarity, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
+
+    def __str_(self):
+        return f'{self.user.username} review for {self.raso.name}'
+
+# Many to many
+
+class Store(models.Model) :
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    raso_varity = models.ManyToManyField(RasoVarity, related_name='stores')
+
+    def __str__(self):
+        return self.name
+
+
+# One to One
+
+class RasoCertificate(models.Model):
+    raso = models.OneToOneField(RasoVarity, on_delete=models.CASCADE, related_name='certificate')
+    certificate_number = models.CharField(max_length=100)
+    issue_date = models.DateTimeField(default=timezone.now)
+    valid_untill = models.DateTimeField()
+
+    def __str__(self):
+        return f'Certificate  for {self.raso.name}'
+
+
+
+
+
+
